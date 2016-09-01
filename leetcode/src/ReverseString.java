c0c48a4254dee737b0826cde7a30d9823f8960d3
Cao, Shiliang <silent.cao@gmail.com>
2016-09-01 23:43:12 +0800

    Reverse String AC

--- leetcode/src/ReverseString.java
+++ leetcode/src/ReverseString.java
@@ -1,4 +1,17 @@
-
 public class ReverseString {
-
+    public static void main(String[] args) {
+        System.out.println(new ReverseString().reverseString("abc"));
+    }
+    public String reverseString(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        char[] array = s.toCharArray();
+        for (int i = 0; i < array.length / 2; i ++) {
+            char tmp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = tmp;
+        }
+        return new String(array);
+    }
 }
