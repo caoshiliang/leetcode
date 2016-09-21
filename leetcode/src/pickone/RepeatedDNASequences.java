f1fff9868e1a8daf1248123707a2cf4174141893
Cao, Shiliang <shiliang.cao@emc.com>
2016-09-21 18:13:15 +0800

    add repreated dna sequences case

---
+++ leetcode/src/pickone/RepeatedDNASequences.java
@@ -0,0 +1,31 @@
+package pickone;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/repeated-dna-sequences/
+ * @author caos1
+ * Key Points:
+ *     1. User set to detect repeat
+ */
+public class RepeatedDNASequences {
+    public List<String> findRepeatedDnaSequences(String s) {
+        if (s.isEmpty() || s.length() < 11) {
+            return new ArrayList<String>();
+        }
+        Set<String> result  = new HashSet<String>();
+        Set<String> set = new HashSet<String>();
+        for (int i = 0; i <= s.length() - 10; i ++) {
+            String tmp = s.substring(i, i + 10);
+            if (set.contains(tmp)) {
+                result.add(tmp);
+            } else {
+                set.add(tmp);
+            }
+        }
+        return new ArrayList<String>(result);
+    }
+}
