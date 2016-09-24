54a339c0e64f76937cde2d0e57810a13a546a0a9
Cao, Shiliang <silent.cao@gmail.com>
2016-09-24 19:57:52 +0800

    modify solution of IntegerReplacement

--- leetcode/src/easy/IntegerReplacement.java
+++ leetcode/src/easy/IntegerReplacement.java
@@ -1,9 +1,30 @@
 package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/integer-replacement/
+ * 
  * @author caos1
  *
+ * Key Points:
+ *     1. To save stack space, uset iterative method, not the recursive one;
+ *     2. To save time, use bit hifting instead of dividing
  */
 public class IntegerReplacement {
-
+    public int integerReplacement(int n) {
+        int count = 0;
+        while (n != 1) {
+            if ((n & 1) == 0) {
+                n >>>= 1;
+            } else if (((n >>> 1) & 1) == 0 || n == 3) {
+                n --;
+            } else {
+                n ++;
+            }
+            count++;
+        }
+        return count;
+    }
 }
