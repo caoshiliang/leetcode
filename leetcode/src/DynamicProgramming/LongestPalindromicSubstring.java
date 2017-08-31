11e285ee1467b1754e88bf22761c0d43f3c03d15
Cao, Shiliang <shiliang.cao@emc.com>
2017-08-31 19:51:56 +0800

    add 2 leet code quetion answers

---
+++ leetcode/src/DynamicProgramming/LongestPalindromicSubstring.java
@@ -0,0 +1,43 @@
+package DynamicProgramming;
+
+/**
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ * 
+ * @author caos1
+ *
+ * Key points:
+ *  1. induction formula;
+ *  2. Iterative direction.
+ */
+public class LongestPalindromicSubstring {
+    
+public String longestPalindrom(String s) {
+        
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+        if (s.length() == 1) {
+            return s;
+        }
+
+        char[] cs = s.toCharArray();
+        int len = s.length();
+        boolean[][] isPal = new boolean[len][len];
+
+        int start = 0, end = 0;
+
+        for (int i = len - 1; i >= 0; i --) {
+            for (int j = len - 1; j >= i; j --) {
+                if (i == j || (i + 1 == j && cs[i] == cs[j]) || (cs[i] == cs[j] && isPal[i + 1][j - 1])) {
+                    isPal[i][j] = true;
+                    if (j - i > end - start) {
+                        start = i;
+                        end = j;
+                    }
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+}
---
+++ leetcode/src/pickone/StringToInteger.java
@@ -0,0 +1,58 @@
+package pickone;
+
+/**
+ * Are you fucking kidding me?
+ * https://leetcode.com/problems/string-to-integer-atoi/description/#
+ * @author caos1
+ *
+ * Key points:
+ *  Just try and try and try until all test cases are covered;
+ */
+public class StringToInteger {
+
+    public static void main(String[] args) {
+//        System.out.println(new StringToInteger().myAtoi("2147483648"));
+        int a = 214748364;
+        int b = a * 10 + 8;
+        System.out.print((b - 8) / 10);
+    }
+
+    public int myAtoi(String str) {
+        int sum = 0;
+        int signal = 1;
+        boolean metValidChar = false;
+        for (int i = 0; i < str.length(); i ++) {
+            char bit = str.charAt(i);
+            if (bit == ' ') {
+                if (metValidChar) {
+                    return sum;
+                }
+                continue;
+            }
+            metValidChar = true;
+            if (bit == '+' || bit == '-') {
+                if (i < str.length() - 1 && (str.charAt(i + 1) == '-' || str.charAt(i + 1) == '+')) {
+                    return 0;
+                }
+                if (bit == '-') {
+                    signal = -1;
+                }
+                continue;
+            }
+            if (bit < '0' || bit > '9') {
+                return sum * signal;
+            }
+            int prev = sum;
+            sum *= 10;
+            sum += bit - '0';
+            if (sum < 0 || (sum - (bit - '0')) / 10 != prev) {
+                if (signal == 1) {
+                    return Integer.MAX_VALUE;
+                } else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+        }
+        return sum * signal;
+    }
+}
