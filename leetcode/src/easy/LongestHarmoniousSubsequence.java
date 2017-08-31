66cda0fd105e08bd10db1ac8f85d1a34e3331737
silent.cao@gmail.com <shiliang.cao@emc.com>
2017-08-31 20:40:33 +0800

    add one case

---
+++ leetcode/src/easy/LongestHarmoniousSubsequence.java
@@ -0,0 +1,32 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/longest-harmonious-subsequence/description/
+ * 
+ * @author caos1
+ * Key points:
+ *  1. Understand definition of harmonious sequence: only have 2 elements whose difference is 1
+ *  2. Use hashmap
+ *  3. Try to be simple
+ */
+public class LongestHarmoniousSubsequence {
+    public static void main(String[] args) {
+        System.out.println(new LongestHarmoniousSubsequence().findLHS(new int[]{1,3,2,2,5,2,3,7}));
+    }
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int num : nums) {
+            m.put(num, m.getOrDefault(num, 0) + 1); // simple point
+        }
+        int max = 0;
+        for (int key : m.keySet()) {
+            if (m.containsKey(key + 1)) {
+                max = Math.max(max, m.get(key) + m.get(key + 1)); // simple point
+            }
+        }
+        return max;
+    }
+}
